db15fe40f92adc753fa0b2338f8eeb55c7b31a1e
ColdRollJoe <joseph.jr2522@gmail.com>
2018-09-09 14:46:13 -0400

    Updated Gas Controller to have hidden key and removed index mapping

--- src/main/java/com/finalprojectridingshotgun/FinalProjectRidingShotgun/gas/controller/GasController.java
+++ src/main/java/com/finalprojectridingshotgun/FinalProjectRidingShotgun/gas/controller/GasController.java
@@ -2,6 +2,7 @@ package com.finalprojectridingshotgun.FinalProjectRidingShotgun.gas.controller;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,17 +14,17 @@ import com.finalprojectridingshotgun.FinalProjectRidingShotgun.gas.entity.Statio
 
 @Controller
 public class GasController {
+	
+	@Value("${gas.key}")
+	String gasID;
 
-//	@RequestMapping("/")
-//	public ModelAndView index() {
-//		return new ModelAndView ("index");
-//	}
+
 	
 	//This method finds the last gas station that has the most expensive gas in the array that is pulled from a 1 mile radius from a location.
 	@RequestMapping("gasprice")
 	public ModelAndView regGasPrice(@RequestParam("lat") Double lat, @RequestParam("lng") Double lng) {
 		RestTemplate restTemplate = new RestTemplate();
-		GasStations station = restTemplate.getForObject("http://api.mygasfeed.com/stations/radius/" + lat + "/" + lng + "/1.0/reg/price/(replace with application file info for key).json", GasStations.class);
+		GasStations station = restTemplate.getForObject("http://api.mygasfeed.com/stations/radius/" + lat + "/" + lng + "/1.0/reg/price/" + gasID + ".json", GasStations.class);
 		ArrayList<StationOptions> priceForCost = station.getChosenStation();
 		priceForCost.get(priceForCost.size() - 1).getGasPrice();
 		
