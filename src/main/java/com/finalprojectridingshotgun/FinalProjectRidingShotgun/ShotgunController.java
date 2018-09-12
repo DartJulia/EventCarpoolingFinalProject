39e94ebc26e460f00597920b9aa71ab8e143c802
John Brinker <jkbrinker@gmail.com>
2018-09-12 11:43:31 -0400

    added user from session attribute

--- src/main/java/com/finalprojectridingshotgun/FinalProjectRidingShotgun/ShotgunController.java
+++ src/main/java/com/finalprojectridingshotgun/FinalProjectRidingShotgun/ShotgunController.java
@@ -50,8 +50,8 @@ public class ShotgunController {
 	}
 
 	@RequestMapping("/pullevent/{id}/{title}")
-	public ModelAndView pullEvent(@PathVariable("id") String id, @PathVariable("title") String title, User user) {
-
+	public ModelAndView pullEvent(@PathVariable("id") String id, @PathVariable("title") String title, HttpSession session) {
+		User user = (User) session.getAttribute("user_name");
 		Ride eventIdAndTitleToAdd = new Ride(id, title, user.getUser_id());
 		System.out.println(eventIdAndTitleToAdd.getEventid());
 		System.out.println(eventIdAndTitleToAdd.getEventtitle());
