300982ddab1193d7e7aaa84e52642e21617a196a
John Brinker <jkbrinker@gmail.com>
2018-09-13 14:38:01 -0400

    one more file

---
+++ src/main/java/com/finalprojectridingshotgun/FinalProjectRidingShotgun/repo/UserRideRepository.java
@@ -0,0 +1,7 @@
+package com.finalprojectridingshotgun.FinalProjectRidingShotgun.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRideRepository extends JpaRepository<UserRide, Long> {
+
+}
